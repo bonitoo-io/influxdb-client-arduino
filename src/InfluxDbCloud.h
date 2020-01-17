65a996d2efce49a1a42d237b4a015ee89b74bde3
vlastahajek <vlastimil.hajek@bonitoo.io>
2020-01-17 15:14:56 +0100

    Updated source licence

--- src/InfluxDbCloud.h
+++ src/InfluxDbCloud.h
@@ -4,7 +4,7 @@
  * 
  * MIT License
  * 
- * Copyright (c) 2018-2020 Tobias Schürg
+ * Copyright (c) 2020 InfluxData
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
